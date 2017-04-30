68c486aaf9f5f4fe4564eae369c00272816ddd81
Mihai Darie <mihai_darie2@yahoo.com>
2017-04-30 12:21:16 +0300

    session idle timer

--- js/header.js
+++ js/header.js
@@ -3,12 +3,86 @@ function loadHeader() {
     var isUserLoggedInLoad = checkCookie();
     setLoginButtonText(isUserLoggedInLoad);
     wireupHeaderButtons();
+    setTimeout(function() {
+        wireupSessionTimer();
+    }, 2000);
+}
+
+function wireupSessionTimer() {
+    // binds to document, 3 minutes
+    //$.idleTimer(3 * 60 * 1000);
+
+    $.idleTimer(5 * 1000);
+
+    $(document).on("idle.idleTimer", function(event, elem, obj){
+        // function you want to fire when the user goes idle
+        alert('idle!!!');
+
+
+        // todo: show popup warning
+        $( "#dialog-confirm" ).dialog({
+            resizable: false,
+            height: "auto",
+            width: 400,
+            modal: true,
+            buttons: {
+                "Da": function() {
+                    $( this ).dialog( "close" );
+                    // refresh cookie
+                    // function you want to fire when the user becomes active again
+                    // refresh login cookie
+                    setLoginCookie();
+
+                    // restore initial idle state, and restart the timer
+                    // returns: jQuery
+                    $( document ).idleTimer("reset");
+                },
+                "Nu": function() {
+                    $( this ).dialog( "close" );
+                    // remove login cookie
+                    removeLoginCookie(); 
+                    // stop the timer, removes data, removes event bindings
+                    // to come back from this you will need to instantiate again
+                    // returns: jQuery
+                    $( document ).idleTimer("destroy");
+                }
+            }
+        });
+
+
+        // remove login cookie
+        removeLoginCookie();
+
+        // stop the timer, removes data, removes event bindings
+        // to come back from this you will need to instantiate again
+        // returns: jQuery
+        $( document ).idleTimer("destroy");
+    });
+
+    $(document).on( "active.idleTimer", function(event, elem, obj, triggerevent){
+        alert('reactivated');
+       
+        // function you want to fire when the user becomes active again
+        // refresh login cookie
+        setLoginCookie();
+
+        // restore initial idle state, and restart the timer
+        // returns: jQuery
+        $( document ).idleTimer("reset");
+    });
+
+}
+
+function wireupHeaderButtons() {
+     $(".promotion button").click(function() {
+        var className = this.firstChild.data;
+        window.location.href = "class.html?name=" + className;
+    });
 
     $("#loginOK").click(function() {
-
         var isUserLoggedIn = checkCookie();
         if(isUserLoggedIn == true) {
-            Cookies.remove('login');
+            removeLoginCookie();
             isUserLoggedIn = checkCookie();
             setLoginButtonText(isUserLoggedIn);
         }
@@ -18,10 +92,8 @@ function loadHeader() {
             var loginDetails = validateCredentials(username, password);
 
             if(loginDetails.IsLoginOk === true) {
-                var in30Minutes = 1/48;
-                Cookies.set('login', { person: loginDetails.UserId + loginDetails.Class }, { expires: in30Minutes });
-
-                // todo: make automatic logout mechanism + automatic cookie lifetime extension
+                var in3Minutes30Seconds = 1/192;
+                setLoginCookie();
 
                 isUserLoggedIn = checkCookie();
                 setLoginButtonText(isUserLoggedIn);
@@ -33,11 +105,12 @@ function loadHeader() {
     });
 }
 
-function wireupHeaderButtons() {
-     $(".promotion button").click(function() {
-        var className = this.firstChild.data;
-        window.location.href = "class.html?name=" + className;
-    });
+function removeLoginCookie() {
+    Cookies.remove('login');
+}
+
+function setLoginCookie() {
+    Cookies.set('login', { person: loginDetails.UserId + loginDetails.Class }, { expires: in3Minutes30Seconds });
 }
 
 function setLoginButtonText(isUserLoggedIn) {
