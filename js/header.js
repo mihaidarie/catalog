6a4a5d885330458fca1b8694eae2d47a105a6988
Mihai Darie <mihai_darie2@yahoo.com>
2017-05-02 21:40:06 +0300

    login timeout and refresh

--- js/header.js
+++ js/header.js
@@ -12,65 +12,61 @@ function wireupSessionTimer() {
     // binds to document, 3 minutes
     //$.idleTimer(3 * 60 * 1000);
 
-    $.idleTimer(5 * 1000);
+    $.idleTimer(15 * 1000);
 
     $(document).on("idle.idleTimer", function(event, elem, obj){
-        // function you want to fire when the user goes idle
+        var isUserLoggedIn = checkCookie();
+            
         alert('idle!!!');
 
+        if(isUserLoggedIn == true) {
+            // function you want to fire when the user goes idle
 
-        // todo: show popup warning
-        $( "#dialog-confirm" ).dialog({
-            resizable: false,
-            height: "auto",
-            width: 400,
-            modal: true,
-            buttons: {
-                "Da": function() {
-                    $( this ).dialog( "close" );
-                    // refresh cookie
-                    // function you want to fire when the user becomes active again
-                    // refresh login cookie
-                    setLoginCookie();
+            $('#alert-session').show();
 
-                    // restore initial idle state, and restart the timer
-                    // returns: jQuery
-                    $( document ).idleTimer("reset");
-                },
-                "Nu": function() {
-                    $( this ).dialog( "close" );
+            // 10 seconds timeout till we logout
+            setTimeout(function () {
+                var isIdle = $(document).idleTimer("isIdle");
+                if(isIdle) {
                     // remove login cookie
-                    removeLoginCookie(); 
+                    removeLoginCookie();
+    
                     // stop the timer, removes data, removes event bindings
                     // to come back from this you will need to instantiate again
                     // returns: jQuery
-                    $( document ).idleTimer("destroy");
+                    $(document).idleTimer("destroy");
+
+                    window.location.href = "index.html";
                 }
-            }
-        });
-
-
-        // remove login cookie
-        removeLoginCookie();
-
-        // stop the timer, removes data, removes event bindings
-        // to come back from this you will need to instantiate again
-        // returns: jQuery
-        $( document ).idleTimer("destroy");
+            }, 10 * 1000);
+        }
     });
 
-    $(document).on( "active.idleTimer", function(event, elem, obj, triggerevent){
+    $(document).on("active.idleTimer", function(event, elem, obj, triggerevent) {
         alert('reactivated');
-       
-        // function you want to fire when the user becomes active again
-        // refresh login cookie
-        setLoginCookie();
+        var isUserLoggedIn = checkCookie();
 
-        // restore initial idle state, and restart the timer
-        // returns: jQuery
-        $( document ).idleTimer("reset");
+        if(isUserLoggedIn == false) {
+            var isIdle = $(document).idleTimer("isIdle");
+            $('#alert-session').hide();
+
+            // function you want to fire when the user becomes active again
+            // refresh login cookie
+            refreshLoginCookie();
+
+            // restore initial idle state, and restart the timer
+            // returns: jQuery
+            $(document).idleTimer("reset");
+        }
     });
+}
 
+function refreshLoginCookie() {
+    var isUserLoggedIn = checkCookie();
+    var cookieValue = Cookies.getJSON('login');
+    if(cookieValue && cookieValue.person) {
+        setLoginCookie(cookieValue.person);
+    }
 }
 
 function wireupHeaderButtons() {
@@ -93,7 +89,7 @@ function wireupHeaderButtons() {
 
             if(loginDetails.IsLoginOk === true) {
                 var in3Minutes30Seconds = 1/192;
-                setLoginCookie();
+                setLoginCookie(loginDetails);
 
                 isUserLoggedIn = checkCookie();
                 setLoginButtonText(isUserLoggedIn);
@@ -109,7 +105,7 @@ function removeLoginCookie() {
     Cookies.remove('login');
 }
 
-function setLoginCookie() {
+function setLoginCookie(loginDetails) {
     Cookies.set('login', { person: loginDetails.UserId + loginDetails.Class }, { expires: in3Minutes30Seconds });
 }
 
