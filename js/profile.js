a132345a05ea86f5e4e1dda8d8315dffb24c25b4
Mihai Darie <mihai_darie2@yahoo.com>
2017-05-04 20:03:09 +0300

    profile display page

--- js/profile.js
+++ js/profile.js
@@ -13,6 +13,27 @@ $(document).ready(function() {
 
 function renderProfileData(profileId, profileClass) {
     alert("profile ID: " + profileId);
+
+    var peopleFileName = "database/people/" + className + ".json";
+
+    $.getJSON(peopleFileName, function( data ) {
+      
+        var items = [];
+        $.each(data, function(index, personDetails) {
+
+            items.push( "<p>" + personDetails.Description + "</p>" );
+
+            $.each(personDetails.Profiles, function(index2, profile) {
+
+                var currentProfileId = "personprofile" + profile.Id;
+                
+                if(currentProfileId == profileId) {
+                    
+                }
+            });
+        });
+        
+    });
 }
 
 // id, firstName, lastName, phone, address, country, linkedIn, facebook, occupation, email, description, smallPhotoPath, largePhotoPath
