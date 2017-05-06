472eda4624e27e640405b5ee8591efc714b8ef42
Mihai Darie <mihai_darie2@yahoo.com>
2017-05-06 09:47:13 +0300

    made file upload functional

--- js/upload.js
+++ js/upload.js
@@ -1,63 +1,83 @@
-$('.upload-btn').on('click', function (){
-    $('#upload-input').click();
-    $('.progress-bar').text('0%');
-    $('.progress-bar').width('0%');
-});
+URI.prototype.getParameter = function(key) {
+    var paramValue = this.query(true)[key];
+    return paramValue;
+};
 
-$('#upload-input').on('change', function(){
+$(document).ready(function() {
+   
+    var uri = URI(window.location.href);
+    var profileId = uri.getParameter('id');
+    var profileClass = uri.getParameter('class');
 
-  var files = $(this).get(0).files;
 
-  if (files.length > 0){
-    // create a FormData object which will be sent as the data payload in the
-    // AJAX request
-    var formData = new FormData();
+    $("header").load('header.html', loadHeader);
+    $("footer").load('footer.html');
 
-    // loop through all the selected files and add them to the formData object
-    for (var i = 0; i < files.length; i++) {
-      var file = files[i];
-
-      // add the files to formData object for the data payload
-      formData.append('uploads[]', file, file.name);
-    }
-
-    $.ajax({
-      url: '/upload',
-      type: 'POST',
-      data: formData,
-      processData: false,
-      contentType: false,
-      success: function(data){
-          console.log('upload successful!\n' + data);
-      },
-      xhr: function() {
-        // create an XMLHttpRequest
-        var xhr = new XMLHttpRequest();
-
-        // listen to the 'progress' event
-        xhr.upload.addEventListener('progress', function(evt) {
-
-          if (evt.lengthComputable) {
-            // calculate the percentage of upload completed
-            var percentComplete = evt.loaded / evt.total;
-            percentComplete = parseInt(percentComplete * 100);
-
-            // update the Bootstrap progress bar with the new percentage
-            $('.progress-bar').text(percentComplete + '%');
-            $('.progress-bar').width(percentComplete + '%');
-
-            // once the upload reaches 100%, set the progress bar text to done
-            if (percentComplete === 100) {
-              $('.progress-bar').html('Done');
-            }
-
-          }
-
-        }, false);
-
-        return xhr;
-      }
+    $('#upload-btn').on('click', function (){
+        $('#upload-input').click();
+        $('.progress-bar').text('0%');
+        $('.progress-bar').width('0%');
     });
 
-  }
+    $('#upload-input').on('change', function(){
+
+      var files = $(this).get(0).files;
+
+      if (files.length > 0) {
+        // create a FormData object which will be sent as the data payload in the
+        // AJAX request
+        var formData = new FormData();
+
+        // loop through all the selected files and add them to the formData object
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+
+          // add the files to formData object for the data payload
+          formData.append('uploads[]', file, file.name);
+        }
+
+        $.ajax({
+          url: '/upload',
+          type: 'POST',
+          data: formData,
+          processData: false,
+          contentType: false,
+          success: function(data) {
+              console.log('upload successful!\n' + data);
+          },
+          complete: function() {
+              $('#upload-input').text('');
+          },
+          xhr: function() {
+            // create an XMLHttpRequest
+            var xhr = new XMLHttpRequest();
+
+            // listen to the 'progress' event
+            xhr.upload.addEventListener('progress', function(evt) {
+
+              if (evt.lengthComputable) {
+                // calculate the percentage of upload completed
+                var percentComplete = evt.loaded / evt.total;
+                percentComplete = parseInt(percentComplete * 100);
+
+                // update the Bootstrap progress bar with the new percentage
+                $('.progress-bar').text(percentComplete + '%');
+                $('.progress-bar').width(percentComplete + '%');
+
+                // once the upload reaches 100%, set the progress bar text to done
+                if (percentComplete === 100) {
+                  $('.progress-bar').html('Incarcare reusita!');
+                }
+
+              }
+
+            }, false);
+
+            return xhr;
+          }
+        });
+
+      }
+    });
 });
+
