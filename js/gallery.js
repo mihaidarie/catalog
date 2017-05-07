ccb2d19ff9b6fdf42b7c28d9de198e57ed25b8e3
Mihai Darie <mihai_darie2@yahoo.com>
2017-05-07 23:09:00 +0300

    added gallery links area

--- js/gallery.js
+++ js/gallery.js
@@ -2,5 +2,17 @@ $(document).ready(function() {
     $("header").load('header.html', loadHeader);
     $("footer").load('footer.html');
 
-    
+    $('#freeLinks').elastic();
+
+    loadLinks();
+
+    $('#galleryLinks button').click(function() {
+        alert('saving links!');
+        // todo: save links text if admin is logged-in
+    });
 });
+
+function loadLinks() {
+    // todo: load links and make editable + show/hide save button, if admin is logged-in
+
+}
