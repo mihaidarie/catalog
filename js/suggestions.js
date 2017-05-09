b1a482651aee5158260cb369de1ba9659e0f9ac0
Mihai Darie <mihai_darie2@yahoo.com>
2017-05-09 23:34:36 +0300

    added captcha for suggestion sending

--- js/suggestions.js
+++ js/suggestions.js
@@ -1,30 +1,73 @@
+
+var iNumber;
+
+function generateNumber() {
+    return Math.floor(Math.random() * 10000);
+}
+
 $(document).ready(function() {
     $("header").load('header.html', loadHeader);
     $("footer").load('footer.html');
+    
+    generateCaptcha();
 
-    $('#sendEmail').click(function() {
-        // todo: make some captcha
+    $("#sendEmail").click(function () {
+        if ($("#textInput").val() != iNumber) {
+            $('#emailResult').text('Numar de verificare incorect!');
+        }
+        else {
+            //alert("Correct Input!!!");
+            sendMail();
+        }  
 
-        var firstname = $('#firstname').val();
-        var lastname = $('#lastname').val();
-        var email = $('#email').val();
-        var subject = $('#subject').val();
-        var emailBody = $('#emailBody').val();
-        var postUrl = "/emailadmin?firstname=" + firstname + "&lastname=" + lastname +
-            "&email=" + email + "&subject=" + subject + "&emailbody=" + emailBody;
-
-        $.ajax({
-          url: postUrl,
-          type: 'POST',
-          error: function(jqXHR, textStatus, errorThrown ) {
-            //alert('jqXHR: ' + jqXHR + " textStatus: " + textStatus + " errorThrown: " + errorThrown);
-            $('#emailResult').text('Trimitere esuata! Contactati administratorul.');
-          },
-          complete: function() {
-              console.log("completed!");
-          }
-        });
-        
-        $('#emailResult').text('Trimitere cu succes!');
+        generateCaptcha();
     });
 });
+
+function sendMail() {
+    var firstname = $('#firstname').val();
+    var lastname = $('#lastname').val();
+    var email = $('#email').val();
+    var subject = $('#subject').val();
+    var emailBody = $('#emailBody').val();
+    var postUrl = "/emailadmin?firstname=" + firstname + "&lastname=" + lastname +
+        "&email=" + email + "&subject=" + subject + "&emailbody=" + emailBody;
+
+    $.ajax({
+        url: postUrl,
+        type: 'POST',
+        error: function(jqXHR, textStatus, errorThrown ) {
+            //alert('jqXHR: ' + jqXHR + " textStatus: " + textStatus + " errorThrown: " + errorThrown);
+            $('#emailResult').text('Trimitere esuata! Contactati administratorul.');
+        },
+        complete: function() {
+            console.log("completed!");
+        }
+    });
+    
+    $('#emailResult').text('Trimitere cu succes!');
+}
+
+function generateCaptcha() {
+    iNumber = generateNumber();
+    $("#sendEmail").prop("disabled", true);
+    $("#divGenerateRandomValues").html("<input id='txtNewInput'></input>");            
+    $("#divGenerateRandomValues").css({ "background-image": 'url(../images/captcha.png)', 'width': '100px', 'height': '50px' });  
+    $("#txtNewInput").css({ 'background': 'transparent', 'font-family': 'Arial', 'font-style': 'bold', 'font-size': '40px' });
+    $("#txtNewInput").css({ 'width': '100px', 'border': 'none', 'color': 'black' });
+    $("#txtNewInput").val(iNumber);
+    $("#txtNewInput").prop('disabled', true);
+
+    var wrongInput = function () {  
+        if ($("#textInput").val() != iNumber) {  
+            return true;
+        }  
+        else {  
+            return false;
+        }  
+    };
+
+    $("#textInput").bind('input', function () {                  
+        $("#sendEmail").prop('disabled', wrongInput);  
+    });  
+}
