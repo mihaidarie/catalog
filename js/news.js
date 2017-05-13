48c7ae631da9eebe4fd8f7aa233fef2484364b6c
Mihai Darie <mihai_darie2@yahoo.com>
2017-05-13 12:59:11 +0300

    news saving and removal

--- js/news.js
+++ js/news.js
@@ -1,10 +1,4 @@
-URI.prototype.getParameter = function(key) {
-    var paramValue = this.query(true)[key];
-    return paramValue;
-};
-
 $(document).ready(function() {
-
     $("header").load('header.html', renderNewsForm);
     $("footer").load('footer.html');
 });
@@ -13,29 +7,85 @@ function renderNewsForm() {
     loadHeader();
     loadNews();
 
-    setupFormMode();
+    // todo: add news button
 
     $('#saveNews').click(function() {
-        alert('save news!');
+        var isAdminUserLoggedIn = isAdminLoggedIn();
+
+        if(isAdminUserLoggedIn) {
+
+            var newsList = [];
+            $('textarea[id^="news_"]').each(function(index, itemDetails) {
+                var newsItem = {};
+                var itemId = itemDetails.id.substring(itemDetails.id.indexOf('news_') + 5);
+                newsItem.Id = itemId;
+                newsItem.Description = itemDetails.value;
+                newsList.push(newsItem);
+            });
+
+            var newsJson = JSON.stringify(newsList);
+
+            var postUrl = "/saveNews";
+            $.ajax({
+                url: postUrl,
+                type: 'POST',
+                data: newsJson,
+                contentType: 'application/json',
+                error: function(jqXHR, textStatus, errorThrown ) {
+                    alert('jqXHR: ' + jqXHR + " textStatus: " + textStatus + " errorThrown: " + errorThrown);
+                },
+                success: function() {
+                    console.log("success!");         
+                },
+                complete: function() {
+                    console.log("completed!");      
+                }
+            });
+        }
     });
     
     $('#removeNews').click(function() {
-        
+        var newsIdsList = [];
+
+        $('input[id^="deletenews_"]:checked').each(function(index, currentItem) {
+            var itemId = currentItem.id.substring(currentItem.id.indexOf('deletenews_') + 11);
+            newsIdsList.push(itemId);
+        });
+
+        var postedData = JSON.stringify(newsIdsList);
+
+        var postUrl = "/removeNews";
+        $.ajax({
+            url: postUrl,
+            type: 'POST',
+            data: postedData,
+            contentType: 'application/json',
+            error: function(jqXHR, textStatus, errorThrown ) {
+                alert('jqXHR: ' + jqXHR + " textStatus: " + textStatus + " errorThrown: " + errorThrown);
+            },
+            success: function() {
+                console.log("success!");
+                loadNews();
+            },
+            complete: function() {
+                console.log("completed!");      
+            }
+        });
     });
 }
 
 function loadNews() {
-    var uri = URI(window.location.href);
-    var newsId = uri.getParameter("id");
     var newsFileName = "database/news/news.json";
-
+    
+    $('#newsList').empty();
+    
     $.getJSON(newsFileName, function(data) {
     
         var items = [];
         $.each(data, function(index, newsDetails) {
             if(newsDetails.Id && newsDetails.Id != '' && newsDetails.Description && newsDetails.Description != '') {
                 items.push( "<li><textarea readonly id=news_" + newsDetails.Id + ">" + newsDetails.Description + 
-                "</textarea><input type='checkbox' id='deletenews_'" + newsDetails.Id + "'/>"
+                "</textarea><input type='checkbox' id='deletenews_" + newsDetails.Id + "'/>"
                 +
                 "</li>" );
             }
@@ -43,6 +93,8 @@ function loadNews() {
         
         var allNews = items.join("");
         $(allNews).prependTo("#newsList");
+
+        setupFormMode();
     });
 }
 
@@ -53,24 +105,8 @@ function setupFormMode() {
 
         $('textarea[id^="news_"]').removeAttr('readonly');
         $('#newsEdit').show();
-
-        $('#removeNews').click(function() {
-            alert('remove news!');
-
-
-        });
-    
-        $('#saveNews').click(function() {
-            alert('save news!');
-
-
-        });
     }
-    else {
-        // $('input[type="text"], textarea').attr('readonly','readonly');
-        //$('#newsEdit').hide();
-
-        
+    else {       
         // redirect to root page if not admin
         window.location.href = "index.html";
     }
